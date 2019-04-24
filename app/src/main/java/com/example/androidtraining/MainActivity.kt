7c647a3ceb58a725dbc6d368f49ff4517ed67c14
Phanthony <anthonyphania216@gmail.com>
2019-04-23 19:15:25 -0500

    Rearranged views around to fit in landscape mode

--- app/src/main/java/com/example/androidtraining/MainActivity.kt
+++ app/src/main/java/com/example/androidtraining/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.androidtraining
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,27 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    fun displayName(view: View){
+        var firstName = ""
+        if (UserNameEditText.text != null){
+            firstName = UserNameEditText.text.toString()
+        }
+        if (firstName != ""){
+            nameDisplayTextView.text = "It's nice to have you."
+            WelcomeTextView.text = "Welcome, $firstName!"
+        }
+        else{
+            nameDisplayTextView.text = "What is your name?"
+            WelcomeTextView.text = "Welcome!"
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
