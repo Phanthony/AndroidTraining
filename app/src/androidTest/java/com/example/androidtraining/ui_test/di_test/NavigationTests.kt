3be4679e2faaeb43e4b8e536b6c5c34eb63eb2f4
Anthony Phan <anthony-phan@uiowa.edu>
2020-03-23 14:45:04 -0500

    Finished Navigation Tests

--- app/src/androidTest/java/com/example/androidtraining/ui_test/di_test/NavigationTests.kt
+++ app/src/androidTest/java/com/example/androidtraining/ui_test/di_test/NavigationTests.kt
@@ -11,6 +11,7 @@ import com.example.androidtraining.R
 import com.example.androidtraining.ui_test.ActivityTestsInterface
 import com.example.androidtraining.ui_test.MockWebServer
 import com.levibostian.recyclerviewmatcher.RecyclerViewMatcher.Companion.recyclerViewWithId
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -35,6 +36,11 @@ class NavigationTests: ActivityTestsInterface() {
     fun setup() {
         diGraph.graph.inject(this)
         vm = viewModelFactory.create(GitHubViewModel::class.java)
+    }
+
+    @After
+    fun clear(){
+        sharedPreferences.edit().clear().commit()
     }
 
     @Test
@@ -64,7 +70,6 @@ class NavigationTests: ActivityTestsInterface() {
         onView(withId(R.id.RepoList)).check(matches(isDisplayed()))
         onView(withId(R.id.login_dest)).perform(click())
         onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
-        sharedPreferences.edit().clear().commit()
     }
 
     @Test
@@ -77,7 +82,148 @@ class NavigationTests: ActivityTestsInterface() {
         onView(withId(R.id.login_dest)).perform(click())
         onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
             click())
-        onView(withId(R.id.IssueCommentList)).check(matches(isDisplayed()))
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun LoginToRepo(){
+        setTellerStateEmpty(vm)
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.repo_dest)).perform(click())
+        onView(withId(R.id.RepoList)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun LoginToLogin(){
+        setTellerStateEmpty(vm)
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun LoginToIssues(){
+        setTellerStateEmpty(vm)
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun LoginToIssueComments(){
+        setTellerStateEmpty(vm,issue = false)
+        setTellerIssue(vm, listOf(testRepoIssue()))
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
+            click())
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueToRepo(){
+        setTellerStateEmpty(vm)
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
+        onView(withId(R.id.repo_dest)).perform(click())
+        onView(withId(R.id.RepoList)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueToLogin(){
+        setTellerStateEmpty(vm)
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
         sharedPreferences.edit().clear().commit()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueToIssue(){
+        setTellerStateEmpty(vm)
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueToIssueComment(){
+        setTellerStateEmpty(vm,issue = false)
+        setTellerIssue(vm, listOf(testRepoIssue()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
+        onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
+            click())
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueCommentToRepo(){
+        setTellerStateEmpty(vm,issue = false)
+        setTellerIssue(vm, listOf(testRepoIssue()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
+            click())
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.repo_dest)).perform(click())
+        onView(withId(R.id.RepoList)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueCommentToLogin(){
+        setTellerStateEmpty(vm,issue = false)
+        setTellerIssue(vm, listOf(testRepoIssue()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
+            click())
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+        sharedPreferences.edit().clear().commit()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.gitHubLoginDisplayText)).check(matches(withText("Log into GitHub")))
+        onView(withId(R.id.userPassLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun IssueCommentToIssue(){
+        setTellerStateEmpty(vm,issue = false)
+        setTellerIssue(vm, listOf(testRepoIssue()))
+        sharedPreferences.edit().putString("access_token", "test token").commit()
+        launchMainActivity()
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(recyclerViewWithId(R.id.IssueList).itemViewAtIndex(0)).perform(
+            click())
+        onView(withId(R.id.issueCommentLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.login_dest)).perform(click())
+        onView(withId(R.id.IssueList)).check(matches(isDisplayed()))
     }
 }
